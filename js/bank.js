f68affc5a08a058e45fb86a920dfc8843c3d957c
Durjoy-dcp <unknownccxv@gmail.com>
2022-08-18 07:46:08 +0600

    balance updates

--- js/bank.js
+++ js/bank.js
@@ -1,7 +1,15 @@
 document.getElementById('depoist-btn').addEventListener('click', function () {
     // console.log("its clicked");
     const depositeField = document.getElementById('deposite-field');
-    console.log(depositeField.value);
+
     const totalDeposite = document.getElementById('total-deposite');
-    totalDeposite.innerText = depositeField.value;
+    totalDeposite.innerText = parseFloat(depositeField.value) + parseFloat(totalDeposite.innerText);
+
+    const totalBalance = document.getElementById('total-balance');
+    console.log(totalBalance);
+    totalBalance.innerText = parseFloat(depositeField.value) + parseFloat(totalBalance.innerText);
+    depositeField.value = '';
+
+
+
 })
